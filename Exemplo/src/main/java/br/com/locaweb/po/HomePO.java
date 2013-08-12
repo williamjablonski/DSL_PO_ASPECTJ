84adece6d7d2128001b5bfdf615167e820657dd9
Eduardo Freitas <eduardo.freitas@locaweb.com.br>
2013-08-12 16:25:16 -0300

    Ajuste nos testes

--- Exemplo/src/main/java/br/com/locaweb/po/HomePO.java
+++ Exemplo/src/main/java/br/com/locaweb/po/HomePO.java
@@ -5,21 +5,20 @@ import br.com.locaweb.config.SeleniumEduardo;
 
 public class HomePO extends SeleniumEduardo {
 
-	public void validarSucessoDaBusca(String artista) throws InterruptedException {
-		for (int second = 0;; second++) {
-			if (second >= 5) fail("timeout");
-			try { if (selenium.isTextPresent("Você buscou por " + artista)) break; } catch (Exception e) {}
-			Thread.sleep(1000);
-		}
+	public void abrirHomeDaRadio() {
+		selenium.open("/");
 	}
-
+	
 	public void buscarArtistaBotao(String artista) {
 		selenium.type("id=q", artista);
 		selenium.click("id=submitbutton");
 	}
-
-	public void abrirHomeDaRadio() {
-		selenium.open("/");
+	
+	public void validarSucessoDaBusca(String artista) throws InterruptedException {
+		for (int second = 0;; second++) {
+			if (second >= 5) fail("timeout");
+			try { if (selenium.isTextPresent("Você buscou por " + artista.toLowerCase())) break; } catch (Exception e) {}
+			Thread.sleep(1000);
+		}
 	}
-
 }
--- Exemplo/src/test/java/br/com/locaweb/suite/BuscaDSL.java
+++ Exemplo/src/test/java/br/com/locaweb/suite/BuscaDSL.java
@@ -5,18 +5,16 @@ import org.junit.Test;
 import br.com.locaweb.po.HomePO;
 
 
-public class BuscaDSL extends HomePO {
-
-		@Test
-		public void testBuscarpeloBotao() throws Exception {
-
-			for (int i = 1; i < planilha.getLinhas(); i++, planilha.setLinhaAtual(planilha.getLinhaAtual() + 1)) {
-				
-				abrirHomeDaRadio();
-				buscarArtistaBotao(planilha.coluna("Artista"));
-				validarSucessoDaBusca(planilha.coluna("Artista"));
-				
-			}
+public class BuscaDSL extends HomePO
+{
+	@Test
+	public void testBuscarArtista() throws Exception 
+	{
+		for (int i = 1; i < planilha.getLinhas(); i++, planilha.setLinhaAtual(planilha.getLinhaAtual() + 1)) 
+		{
+			abrirHomeDaRadio();
+			buscarArtistaBotao(planilha.coluna("Artista"));
+			validarSucessoDaBusca(planilha.coluna("Artista"));
 		}
-
+	}
 }
