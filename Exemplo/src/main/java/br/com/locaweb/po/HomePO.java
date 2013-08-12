3b77d7964d14d0df0da2760835988d1afe355060
Eduardo Freitas <eduardo.freitas@locaweb.com.br>
2013-08-12 17:36:30 -0300

    Inserçaõ da classe de suite do maven

--- Exemplo/src/main/java/br/com/locaweb/po/HomePO.java
+++ Exemplo/src/main/java/br/com/locaweb/po/HomePO.java
@@ -21,7 +21,7 @@ public class HomePO extends SeleniumEduardo {
 	
 	public void validarSucessoDaBusca(String artista) throws InterruptedException {
 		for (int second = 0;; second++) {
-			if (second >= 5) fail("timeout");
+			if (second >= 30) fail("timeout");
 			try { if (selenium.isTextPresent("Você buscou por " + artista.toLowerCase())) break; } catch (Exception e) {}
 			Thread.sleep(1000);
 		}
